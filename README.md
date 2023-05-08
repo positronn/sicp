df377c33a1bb4c899f5990e7c88c580ba5c1ad74
positronn <marcodelossantos@protonmail.com>
2023-05-07 19:21:24 -0600

    Defining variables. Prefix notation.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sicp
 My notes and attempts on the book "Structure and Interpretation of Computer Programs", 2nd Edition.
+
+
+### Running files
+To execute a file do:
+```
+scheme --load "<filename>.scm"
+```
---
+++ notes/definition.scm
@@ -0,0 +1,9 @@
+; Comments in scheme begin with ;
+
+; to define variable we use the clause `define`
+; to do evaluation we enclose in parentheses
+; We say that the name identifies a variable whose value is the object.
+(define size 2)
+; we use prefix notation in lisp:
+;    placing the operator to the le of the operands
+(* 5 size)
