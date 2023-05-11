cc367d5ccc0f232d8bc535a3925c23bb7187de18
positronn <marcodelossantos@protonmail.com>
2023-05-11 17:23:57 -0600

    Fibonacci implementations.

---
+++ notes/chapter01/fibonacci-recursive.scm
@@ -0,0 +1,7 @@
+;; fibonacci number generator as recursive procedure
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))
+  ))
